3d44c0fd3a44aad21fe0263209f9adc51080d001
pepe <dapaslan@campusaula.com>
2017-05-03 20:14:51 +0200

    actualizado

--- EDE_A6-Daniel_Pastor_Lanzuela/EjerciciosJUnit/src/LineTest.java
+++ EDE_A6-Daniel_Pastor_Lanzuela/EjerciciosJUnit/src/LineTest.java
@@ -1,33 +1,50 @@
 import static org.junit.Assert.*;
 
-import org.junit.Test;
 import org.junit.Before;
+import org.junit.Test;
 public class LineTest {
 
+	double delta = 0.001;
+	Line theLine;
 	
-	private  double x0=2;
-	private  double y0=5;
-	private  double x1=2;
-	private double y1=5;
+	double x0=1,x1=3,y0=2,y1=4;
 	
-	@Test
+	@Before
+	public void setUp()
+	{
+		theLine= new Line(1,2,3,4);
+		
+	}
+	
+	// claculate de slo`pe of the line@Test
 	public void testgetSlope(){
-		double delta= 0.0001;
-		double resultado = Line.getSlope();
-		double esperado = 0;
-		assertEquals(esperado, resultado, delta);
+		
+		 double valorReal =theLine.getSlope();
+		 double valorEsperado = (4 - 2) / (3 - 1);
+		 
+		 assertEquals(valorEsperado, valorReal,delta);
 	}
 	
 	
+
+
 	@Test
 	public void  testgetDistance(){
-	
 		
+		double valorReal = theLine.getDistance();
+		double valorEsperado = Math.sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0));
+		assertEquals(valorEsperado, valorReal,delta);
 		
 	}
 	
 	@Test
 	public void testparallelTo(){
+		
+		Line theLine2;
+		theLine2 = new Line(1,2,3,4);
+		boolean valorReal = theLine.parallelTo(theLine2);
+		assertTrue(valorReal);
+		
 		
 	}
 }
